a0cbe3f1738d2d06fe7e11d484d819d4dbc58bf3
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2023-04-10 19:24:09 -0300

    criando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.css';
+import Link from 'next/link';
+
+export function Header(){
+    return(
+        <header className={styles.header}>
+            <section className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>
+                            Tarefas <span>+</span>
+                        </h1>
+                    </Link>
+                    <Link href="/dashboard" className={styles.link}>
+                        Meu painel
+                    </Link>
+                </nav>
+
+                <button className={styles.loginButton}>Minha conta</button>
+            </section>
+        </header>
+    )
+}
+
